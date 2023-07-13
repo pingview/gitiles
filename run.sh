a8bf512212a3fa07482e7df7534c537550dedff9
Jia <angersax@sina.com>
2023-07-13 22:19:03 +0800

    feat(run): update script file

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# Refer: https://gerrit.googlesource.com/gitiles/+/refs/tags/v1.2.0/tools/run_dev.sh
+
+set -e
+
+PROPERTIES="--jvm_flag=-Dcom.google.gitiles.configPath=$1"
+PROPERTIES="$PROPERTIES --jvm_flag=-Dcom.google.gitiles.sourcePath=$2"
+
+"bazel-bin/java/com/google/gitiles/dev/dev" $PROPERTIES
